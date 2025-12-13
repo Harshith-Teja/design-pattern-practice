16f0dd93b58a1152e39ffa16f591c562bda232a6
Harshith Teja <harshithtejadontoju@gmail.com>
2025-12-13 21:39:29 +0530

    add factory design pattern

---
+++ factory-design-pattern/src/ShapeFactory.java
@@ -0,0 +1,17 @@
+public class ShapeFactory {
+
+    public static Shape getShape(String type) {
+        if (type == null) {
+            return null;
+        }
+
+        switch (type.toLowerCase()) {
+            case "circle":
+                return new Circle();
+            case "rectangle":
+                return new Rectangle();
+            default:
+                throw new IllegalArgumentException("Unknown shape type");
+        }
+    }
+}
