df7148393bde472808dd205c5bfb30882edf5840
Harshith Teja <harshithtejadontoju@gmail.com>
2025-12-30 21:59:14 +0530

    add command design pattern

---
+++ command-design-pattern/src/Fan.java
@@ -0,0 +1,9 @@
+public class Fan {
+    public void start() {
+        System.out.println("Fan is running");
+    }
+
+    public void stop() {
+        System.out.println("Fan is stopped");
+    }
+}
